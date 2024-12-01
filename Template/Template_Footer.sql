ee52f1aaf9410428c257a0fafd4913d96b1af82e
Guy Glantser <guy@madeiradata.com>
2024-12-01 15:49:42 +0200

    Update Template_Footer.sql

--- Template/Template_Footer.sql
+++ Template/Template_Footer.sql
@@ -6,40 +6,65 @@ SELECT
 	InstanceScore = CAST (ROUND ((1.0 - CAST (SUM (CurrentStateImpact) AS DECIMAL(19,2)) / CAST (SUM (WorstCaseImpact) AS DECIMAL(19,2))) * 100.0 , 0) AS TINYINT)
 FROM
 	#Checks;
+GO
+
+
+SELECT
+	Output_1 = N'Failed Checks that Require Attention:';
+GO
+
 
 SELECT
 	[Check Id]				= CheckId ,
 	[Problem Description]	= Title ,
-	[Requires Attention]	= RequiresAttention ,
 	[Current State Impact]	=
 		CASE CurrentStateImpact
-			WHEN 0 THEN N'None'
 			WHEN 1 THEN N'Low'
 			WHEN 2 THEN N'Medium'
 			WHEN 3 THEN N'High'
 		END ,
 	[Recommendation Effort]	= 
 		CASE RecommendationEffort
-			WHEN 0 THEN N'None'
 			WHEN 1 THEN N'Low'
 			WHEN 2 THEN N'Medium'
 			WHEN 3 THEN N'High'
 		END ,
 	[Recommendation Risk]	=
 		CASE RecommendationRisk
-			WHEN 0 THEN N'None'
 			WHEN 1 THEN N'Low'
 			WHEN 2 THEN N'Medium'
 			WHEN 3 THEN N'High'
 		END ,
-	[Additional Info]		= AdditionalInfo
+	[Additional Info]		= AdditionalInfo ,
+	[Instance Score Impact]	= CAST (ROUND (CAST (CurrentStateImpact AS DECIMAL(19,2)) / (SELECT CAST (SUM (WorstCaseImpact) AS DECIMAL(19,2)) FROM #Checks) * 100.0 , 0) AS TINYINT)
 FROM
 	#Checks
+WHERE
+	RequiresAttention = 1
 ORDER BY
-	#Checks.RequiresAttention		DESC ,
 	#Checks.CurrentStateImpact		DESC ,
 	#Checks.RecommendationEffort	ASC ,
 	#Checks.RecommendationRisk		ASC;
+GO
+
+
+SELECT
+	Output_2 = N'Passed Checks that Don''t Require Attention:';
+GO
+
+
+SELECT
+	[Check Id]				= CheckId ,
+	[Problem Description]	= Title ,
+	[Additional Info]		= AdditionalInfo
+FROM
+	#Checks
+WHERE
+	RequiresAttention = 0
+ORDER BY
+	#Checks.CurrentStateImpact DESC;
+GO
+
 
 IF EXISTS
 	(
@@ -49,6 +74,9 @@ IF EXISTS
 			#Errors
 	)
 BEGIN
+
+	SELECT
+		Output_3 = N'Errors:';
 
 	SELECT
 		CheckId ,
